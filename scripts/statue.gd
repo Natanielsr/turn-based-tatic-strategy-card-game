f21550d0a63af2d77ceececcafcfff2af4de62c7
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-06-20 20:24:34 -0300

    add statue script

---
+++ scripts/statue.gd
@@ -0,0 +1,22 @@
+extends Entity
+
+class_name Statue
+
+@onready var turn_sprite: Sprite2D = $"./TurnSprite"
+
+func _statue_ready() -> void:
+	base_ready()
+	turn_set(game_controller.turn)
+	
+	
+func turn_set(turn):
+	if is_my_turn():
+		show_turn_sprite(true)
+	else:
+		show_turn_sprite(false)
+	
+func _on_changed_turn(turn):
+	turn_set(turn)
+
+func show_turn_sprite(show_sprite):
+	turn_sprite.visible = show_sprite
