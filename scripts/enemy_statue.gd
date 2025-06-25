0426aa3a8b8cc349f270a601d8c3feb2892ae3e2
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-06-25 19:03:57 -0300

    change faction number to enum in get distance statue

--- scripts/enemy_statue.gd
+++ scripts/enemy_statue.gd
@@ -10,8 +10,8 @@ func die():
 	get_tree().paused = true
 	
 func get_distance(pos : Vector2):
-	var faction_area = grid_controller.faction_area(pos)
-	if faction_area == 2: #ENEMY AREA
+	var faction_area = grid_controller.get_faction_area(pos)
+	if faction_area == EntityFaction.ENEMY: #ENEMY AREA
 		return 1
 	else:
 		return 9999
