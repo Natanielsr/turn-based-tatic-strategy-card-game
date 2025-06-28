cc2b8ec5a58f140d3a8dcaaef18a5e11020af38f
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-06-28 15:59:13 -0300

    sorted troops by distance in troop manager

--- scripts/troops_manager.gd
+++ scripts/troops_manager.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 class_name TroopManager
 
+@onready var grid_controller: GridController = $"../Controllers/GridController"
+
 var player_troops = []
 var enemy_troops = []
+
+
 
 func add_troop(troop : MobileTroop):
 	add_child(troop)	
@@ -17,3 +21,21 @@ func remove_troop(troop):
 		player_troops.erase(troop)
 	elif troop.faction == Entity.EntityFaction.ENEMY:
 		enemy_troops.erase(troop)
+		
+func sorted_opponents_by_distance(troop: MobileTroop) -> Array:
+	var distances = []
+	
+	for monster: MobileTroop in player_troops:
+		var distance = grid_controller.get_distance(
+			troop.global_position,
+			monster.global_position
+		)
+		distances.append({ "troop": monster, "distance": distance })
+	
+	distances.sort_custom(func(a, b): return a["distance"] < b["distance"])
+	
+	var sorted_troops = []
+	for entry in distances:
+		sorted_troops.append(entry["troop"])
+	
+	return sorted_troops
