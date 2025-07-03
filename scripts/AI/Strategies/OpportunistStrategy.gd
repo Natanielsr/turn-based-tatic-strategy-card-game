3cf33ea105aa2067cec4e1d218b57a42bf021378
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-03 20:46:17 -0300

    implements opportunity strategy

--- scripts/AI/Strategies/OpportunistStrategy.gd
+++ scripts/AI/Strategies/OpportunistStrategy.gd
@@ -1,39 +1,62 @@
 extends AIStrategy
 
-var selected_troop
+
 var opponents
 
 func play_turn():
+	_turn_start()
+	
 	await spawner.select_and_spawn_best_monster()
-	await mover.move_all()
+	_update_troops_list()
+	await enemy_ai.wait(0.3)
+	await _process_next_troop()
 	
 func move_trop_pos(troop):
-	if ai_finder.in_area_pos(troop):
-		return null
-		
-	selected_troop = troop
 	
-	opponents = troop_manager.sorted_opponents_by_distance(selected_troop)
+	var objective_pos = null
+	if ai_finder.in_attack_area_pos(troop):
+		return null
+	
+	opponents = troop_manager.sorted_opponents_by_distance(current_troop)
 	
 	if opponents.size() == 0:
-		return closest_area_pos()
+		objective_pos = closest_area_pos()
+		if grid_controller.is_achievable_path(current_troop.global_position, objective_pos):
+			return grid_controller.get_last_pos_path_with_walk_points(
+				current_troop.global_position,
+				objective_pos,
+				current_troop.get_current_walk_points())
+		else:
+			return ai_finder.find_nearest_reachable_position(current_troop, objective_pos)
 	
-	if opponent_close_troop():
-		return null
+	#if opponent_close_troop():
+	#	return null
 		
 	for opp in opponents:
 		var is_achievable_troop = grid_controller.is_achievable_troop(
-			selected_troop.global_position,
+			current_troop.global_position,
 			opp.global_position,
 		)
 		
 		if is_achievable_troop:
-			return opp.global_position
+			objective_pos = opp.global_position
 			
-	return closest_area_pos()
+			var final_pos  = grid_controller.get_pos_to_troop(
+				current_troop.global_position,
+				objective_pos,
+				current_troop.get_current_walk_points())
+				
+			print("is_achievable_troop pos: ",final_pos)
+				
+			return final_pos
+		else:
+			continue
+			
+	objective_pos = opponents[0].global_position
+	return ai_finder.find_nearest_reachable_position(current_troop, objective_pos)
 	
 func closest_area_pos():
-	var closest_area = ai_finder.closest_area_to_attack(selected_troop)
+	var closest_area = ai_finder.closest_area_to_attack(current_troop)
 	if closest_area:
 		return closest_area.global_position
 	else:
@@ -43,7 +66,7 @@ func closest_area_pos():
 			
 func opponent_close_troop():
 	var distance = grid_controller.get_distance(
-		selected_troop.global_position,
+		current_troop.global_position,
 		opponents[0].global_position #first opponent is the close
 		)
 	
