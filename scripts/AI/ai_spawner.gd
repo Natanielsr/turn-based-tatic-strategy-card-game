a15d4063cc0f43c60cc7ff024c700a6fec10d4fc
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-03 20:44:25 -0300

    get valid spawn tiles

--- scripts/AI/ai_spawner.gd
+++ scripts/AI/ai_spawner.gd
@@ -7,26 +7,31 @@ class_name AISpawner
 @onready var spawn_points = get_spawn_points()
 @onready var enemy_hand: EnemyHand = $"../EnemyHand"
 @onready var troop_manager: TroopManager = $"../../TroopManager"
+@onready var enemy_statue: EnemyStatue = $"../../Statues/EnemyStatue"
 
 var selected_card
 
 func select_and_spawn_best_monster():
-	await wait(1)
-	selected_card = get_strongest_card()
-	if selected_card:
-		var pos = find_spawn_position()
-		if pos:
-			var _troop : MobileTroop = card_manager.spawn_monster(
-				selected_card, pos, Entity.EntityFaction.ENEMY)
+	await wait(0.3)
+	print("enemy hand: ",enemy_hand.hand)
+	for card in enemy_hand.hand:
+		await wait(0.3)
+		selected_card = get_strongest_card()
+		if selected_card:
+			var pos = find_spawn_position()
+			if pos:
+				var _troop : MobileTroop = card_manager.spawn_monster(
+					selected_card, pos, Entity.EntityFaction.ENEMY)
 
-			enemy_hand.remove_card_from_hand(selected_card)
 
 func get_strongest_card():
-	print(enemy_hand.hand)
+	
 	var strongest_card = null
 	
 	for card in enemy_hand.hand:
 		var card_data = game_controller.card_database.CARDS[card]
+		if card_data.energy_cost > enemy_statue._current_energy:
+			continue
 		if card_data.type == "monster":
 			if strongest_card == null:
 				strongest_card = card_data
@@ -47,4 +52,12 @@ func get_spawn_points():
 	
 func wait(seconds):
 	await get_tree().create_timer(seconds).timeout 
+
+func get_valid_spawn_tiles():
+	var valid_tiles = []
+	for marker in spawn_points:
+		if grid_controller.is_walkable_position(marker.global_position):
+			var tile_pos = grid_controller.get_world_to_tile_pos(marker.global_position)
+			valid_tiles.append(tile_pos)
+	return valid_tiles
 	
