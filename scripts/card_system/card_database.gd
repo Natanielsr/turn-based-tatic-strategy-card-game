d6ac307a3176ccc023788042b45ae3347849edc0
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-03 20:46:29 -0300

    add more cards do card database

--- scripts/card_system/card_database.gd
+++ scripts/card_system/card_database.gd
@@ -1,13 +1,43 @@
 class_name CardDatabase
 
 const CARDS = {
+	"rat": {
+		"card_id": "rat",
+		"type": "monster",
+		"name": "Rat",
+		"energy_cost": 1,
+		"attack": 1,
+		"health": 1,
+		"description": "...",
+		"ability": "..."
+  	},
+	"spider": {
+		"card_id": "spider",
+		"type": "monster",
+		"name": "Spider",
+		"energy_cost": 2,
+		"attack": 2,
+		"health": 2,
+		"description": "...",
+		"ability": "..."
+  	},
+	"wolf": {
+		"card_id": "wolf",
+		"type": "monster",
+		"name": "Wolf",
+		"energy_cost": 3,
+		"attack": 3,
+		"health": 3,
+		"description": "...",
+		"ability": "..."
+  	},
 	"goblin": {
 		"card_id": "goblin",
 		"type": "monster",
 		"name": "Goblin",
-		"energy_cost": 1,
-		"attack": 1,
-		"health": 1,
+		"energy_cost": 4,
+		"attack": 4,
+		"health": 4,
 		"description": "Tropa goblin simples.",
 		"ability": "nothing"
  	},
@@ -15,9 +45,9 @@ const CARDS = {
 		"card_id": "hobgoblin",
 		"type": "monster",
 		"name": "Hobgoblin",
-		"energy_cost": 2,
-		"attack": 2,
-		"health": 3,
+		"energy_cost": 5,
+		"attack": 5,
+		"health": 5,
 		"description": "Entra em modo berserk.",
 		"ability": "berserk"
   	},
