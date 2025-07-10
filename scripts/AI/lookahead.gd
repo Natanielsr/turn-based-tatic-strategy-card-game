cb3c489467c2e59e511ec26b4e39140cb9ef6a33
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-10 16:11:41 -0300

    improve lookahead script

--- scripts/AI/lookahead.gd
+++ scripts/AI/lookahead.gd
@@ -1,6 +1,7 @@
 extends Node2D
+class_name LookAhead
 
-class_name  LookAhead
+const scoreCalculator = preload("res://scripts/AI/score_calculator.gd")
 
 @onready var game_controller: GameController = $"../../Controllers/GameController"
 @onready var troop_manager: TroopManager = $"../../TroopManager"
@@ -12,368 +13,262 @@ class_name  LookAhead
 @onready var enemy_statue: EnemyStatue = $"../../Statues/EnemyStatue"
 @onready var player_statue: PlayerStatue = $"../../Statues/PlayerStatue"
 
+var score_calculator = scoreCalculator.new()
 
 func simulate_moves():
 	var best_score = -INF
 	var best_move = null
 	var possible_moves = get_all_possible_moves()
+	
 	for move in possible_moves:
-		var cloned_state = clone_game_state()
-		apply_move(cloned_state, move)
-		var score = evaluate_move(cloned_state, move)
+		var state = clone_game_state()
+		apply_move(state, move)
+		var score = evaluate_move(state, move)
 		if score > best_score:
 			best_score = score
 			best_move = move
-	print("BEST SCORE: ",best_score)
 	
+	print("BEST SCORE: ", best_score)
 	return best_move
 
-func get_all_possible_moves():
+func simulate_moves_lookahead2():
+	var best_score = -INF
+	var best_moves = []
+	var possible_moves = get_all_possible_moves()
+	
+	for move1 in possible_moves:
+		var state1 = clone_game_state()
+		apply_move(state1, move1)
+		
+		var moves2 = get_all_possible_moves_for_state(state1)
+		if moves2.is_empty():
+			var score = evaluate_move(state1, move1)
+			if score > best_score:
+				best_score = score
+				best_moves = [move1]
+		else:
+			for move2 in moves2:
+				var state2 = state1.duplicate()
+				apply_move(state2, move2)
+				var score = evaluate_move(state2, move2)
+				if score > best_score:
+					best_score = score
+					best_moves = [move1, move2]
+	
+	print("BEST SCORE (lookahead 2): ", best_score)
+	return best_moves
 
+func get_all_possible_moves() -> Array:
 	var moves = []
-
-	#1. Play cards from hand (example: troop cards)
+	
+	# Play cards from hand
 	for card in enemy_hand.hand:
 		if can_play_the_card(card):
 			for tile in get_valid_spawn_tiles():
 				moves.append({
 					"type": "play_card",
 					"card": card,
-					"tile": tile
+					"tile": tile,
+					"monster_id": troop_manager.generate_id(card, Entity.EntityFaction.ENEMY)
 				})
-
-	# 2. Move enemy troops
+	
+	# Move troops
 	for troop in troop_manager.enemy_troops:
 		if troop.can_move():
-			var move_tiles = get_valid_move_tiles(troop)
-			if move_tiles.size() == 0:
-				print(move_tiles)
-			for tile in move_tiles:
+			for tile in get_valid_move_tiles(troop):
 				moves.append({
 					"type": "move_troop",
 					"troop": troop,
-					"tile": tile
+					"tile": tile,
+					"monster_id": troop.name,
+					"troop_pos": troop.get_tile_pos()
 				})
-
-	#3. Attack with enemy troops
+	
+	# Attack with troops
 	for troop in troop_manager.enemy_troops:
 		if troop.can_attack():
 			for target in get_attackable_targets(troop):
 				moves.append({
 					"type": "attack",
 					"troop": troop,
-					"target": target
+					"target": target,
 				})
 	
-
-
 	return moves
 
-func apply_move(cloned_state, move):
+func get_all_possible_moves_for_state(state: Dictionary) -> Array:
+	var moves = []
+	
+	# Play cards from hand
+	for card in state["enemy_hand"]:
+		if can_play_the_card(card["name"]):
+			for tile in state["enemy_statue"]["attack_positions"]:
+				moves.append({
+					"type": "play_card",
+					"card": card["name"],
+					"tile": tile,
+					"monster_id": card_manager.generate_id(card["name"], Entity.EntityFaction.ENEMY)
+				})
+	
+	# Move troops
+	for troop in state["enemy_troops"]:
+		for tile in get_valid_move_tiles_for_state(troop, state):
+			moves.append({
+				"type": "move_troop",
+				"troop": troop,
+				"tile": tile,
+				"troop_pos": troop["pos"]
+			})
+	
+	# Attack with troops
+	for troop in state["enemy_troops"]:
+		for target in get_attackable_targets_for_state(troop, state):
+			moves.append({
+				"type": "attack",
+				"troop": troop,
+				"troop_pos": troop["pos"],
+				"target": target,
+				"target_pos": target["pos"]
+			})
+	
+	return moves
+
+func get_valid_move_tiles_for_state(troop: Dictionary, state: Dictionary) -> Array:
+	var tiles = []
+	var directions = [Vector2i(1,0), Vector2i(-1,0), Vector2i(0,1), Vector2i(0,-1)]
+	
+	for dir in directions:
+		var new_pos = troop["pos"] + dir
+		var occupied = false
+		
+		for other in state["enemy_troops"] + state["player_troops"]:
+			if other["hp"] > 0 and other["pos"] == new_pos:
+				occupied = true
+				break
+		
+		if not occupied:
+			tiles.append(new_pos)
+	
+	return tiles
+
+func get_attackable_targets_for_state(troop: Dictionary, state: Dictionary) -> Array:
+	var targets = []
+	
+	# Check troops
+	for player in state["player_troops"]:
+		if player["hp"] > 0 and troop["pos"].distance_to(player["pos"]) == 1:
+			targets.append(player)
+	
+	# Check statue
+	for pos in state["player_statue"]["attack_positions"]:
+		if troop["pos"] == pos:
+			targets.append(state["player_statue"])
+	
+	return targets
+
+func apply_move(state: Dictionary, move: Dictionary) -> void:
 	match move["type"]:
 		"play_card":
-			#remove card from the virual hand
-			for i in cloned_state["enemy_hand"].size():
-				var card_selected = cloned_state["enemy_hand"][i]
-				if card_selected["name"] == move["card"]:
-					
-					#add a virtual troop in the specified tile
-					cloned_state["enemy_troops"].append({
-						"name":  move["card"],
+			# Remove card from hand
+			for i in state["enemy_hand"].size():
+				var card = state["enemy_hand"][i]
+				if card["name"] == move["card"]:
+					# Add troop to board
+					state["enemy_troops"].append({
+						"name": move["monster_id"],
 						"pos": move["tile"],
-						"hp": card_selected.life,
-						"attack_points" : card_selected.attack
-						# ... other attributes
+						"hp": card.life,
+						"attack_points": card.attack
 					})
-					
-					cloned_state["enemy_hand"].remove_at(i)
+					state["enemy_hand"].remove_at(i)
 					break
-			
-			
+		
 		"move_troop":
-			#move the virtual troop to the specified tile
-			for troop in cloned_state["enemy_troops"]:
-				if troop["pos"] == move["troop"].get_tile_pos():
+			# Move troop
+			for troop in state["enemy_troops"]:
+				if troop["pos"] == move["troop_pos"]:
 					troop["pos"] = move["tile"]
 					break
 		
 		"attack":
-			#Reduce the life points of the target troops
-			var attacker = null
-			for troop in cloned_state["enemy_troops"]:
-				if troop["pos"] == move["troop"].get_tile_pos():
-					attacker = troop
-					break
+			# Process attack
+			var attacker = state["enemy_troops"].filter(func(t): return t["pos"] == move["troop"].get_tile_pos())[0]
 			
 			if move["target"] is Statue:
-				var current_player_statue = cloned_state["player_statue"]
-				current_player_statue["hp"] -= attacker["attack_points"]
+				state["player_statue"]["hp"] -= attacker["attack_points"]
 			else:
-				for target in cloned_state["player_troops"]:
-					if target["pos"] == move["target"].get_tile_pos():
-						target["hp"] -= attacker["attack_points"]
-						attacker["hp"] -= target["attack_points"]
-						#if target["hp"] <= 0:
-							#cloned_state["player_troops"].erase(target)  # Remove dead troop
-							
-						#if attacker["hp"] <= 0:
-							#cloned_state["enemy_troops"].erase(attacker)
-						break
+				var result = state["player_troops"].filter(func(t): return t["pos"] == move["target"].get_tile_pos())
+				if result.size() > 0:
+					var target = result[0]
+					target["hp"] -= attacker["attack_points"]
+					attacker["hp"] -= target["attack_points"]
+				else:
+					push_error("TARGET NOT FOUND")
 
-	
-func clone_game_state():
+func clone_game_state() -> Dictionary:
 	var state = {
 		"enemy_hand": [],
 		"enemy_troops": [],
 		"player_troops": [],
 		"enemy_statue": {
 			"hp": enemy_statue.current_life_points,
-			"attack_positions": get_valid_spawn_tiles(),
-			# ... outros atributos
+			"initial_hp": enemy_statue.current_life_points,
+			"attack_positions": get_valid_spawn_tiles()
 		},
 		"player_statue": {
 			"hp": player_statue.current_life_points,
+			"initial_hp": player_statue.current_life_points,
 			"attack_positions": ai_finder.get_attack_player_tiles(),
-			# ... outros atributos
-		},
-		# ... outros dados relevantes
+			"pos" : Vector2i(0,0)
+		}
 	}
-
+	
+	# Clone hand
 	for card in enemy_hand.hand:
 		var card_data = game_controller.card_database.CARDS[card]
 		state["enemy_hand"].append({
 			"name": card_data.card_id,
-			"attack" : card_data.attack,
-			"life" : card_data.health
-			# ... outros atributos
+			"attack": card_data.attack,
+			"life": card_data.health
 		})
 	
-	for troop : MobileTroop in troop_manager.enemy_troops:
+	# Clone troops
+	for troop in troop_manager.enemy_troops:
 		state["enemy_troops"].append(create_mobile_obj(troop))
-		
-	for troop : MobileTroop in troop_manager.player_troops:
+	
+	for troop in troop_manager.player_troops:
 		state["player_troops"].append(create_mobile_obj(troop))
-		
-	# Repita para player_troops, cartas, etc.
+	
 	return state
-	
-func create_mobile_obj(troop : MobileTroop):
+
+func create_mobile_obj(troop: MobileTroop) -> Dictionary:
 	return {
-			"card_id" : troop.card_id,
-			"pos": troop.get_tile_pos(),
-			"hp": troop.current_life_points,
-			"attack_points" : troop.attack_points
-			
-			# ... outros atributos
-		}
-	
-func evaluate_move(game_state, move) -> int:
-	var score = 0
-	score += score_alive_troops(game_state)
-	score += score_total_life(game_state)
-	score += score_attack_result(game_state, move)
-	score += score_area_control(game_state)
-	score += score_statue_damage(game_state)
-	score += score_victory_defeat(game_state)
-	score += score_hunt_weaker_targets(game_state)
-	score += score_approach_invader(game_state)
-	score += score_attack_invader(game_state, move) 
-	#score += score_escape_weak_from_strong(game_state, move)
-	score += score_approach_enemy_defense_statue(game_state)
-	score += score_kill_enemy_defense_statue(game_state, move)
+		"name": troop.name,
+		"card_id": troop.card_id,
+		"pos": troop.get_tile_pos(),
+		"hp": troop.current_life_points,
+		"attack_points": troop.attack_points,
+		"monster_id": troop_manager.generate_id(troop.card_id, Entity.EntityFaction.ENEMY)
+	}
 
+func evaluate_move(game_state: Dictionary, move: Dictionary) -> int:
+	return score_calculator.calculate_total_score(game_state, move)
 
-	return score
-
-func score_alive_troops(game_state) -> int:
-	var score = 0
-	score += game_state["enemy_troops"].filter(func(t): return t["hp"] > 0).size() * 10
-	score -= game_state["player_troops"].filter(func(t): return t["hp"] > 0).size() * 10
-	return score
-
-func score_total_life(game_state) -> int:
-	var score = 0
-	for troop in game_state["enemy_troops"]:
-		score += troop["hp"]
-	for troop in game_state["player_troops"]:
-		score -= troop["hp"]
-	return score
-
-func score_attack_result(game_state, move) -> int:
-	var score = 0
-	if move["type"] == "attack":
-		if move["target"] is Statue:
-			return 0
-			
-		for target in game_state["player_troops"]:
-			if target["pos"] == move["target"].get_tile_pos(): #se for statue nao faz esse calculo
-				var attacker = null
-				for t in game_state["enemy_troops"]:
-					if t["pos"] == move["troop"].get_tile_pos():
-						attacker = t
-						break
-				if target["hp"] <= 0 and attacker and attacker["hp"] > 0:
-					score += 40
-				elif attacker["hp"] <= 0 and target["hp"] > 0:
-					score -= 40
-	return score
-
-func score_area_control(game_state) -> int:
-	var score = 0
-	for troop in game_state["enemy_troops"]:
-		var min_dist = INF
-		for attack_pos in game_state["player_statue"]["attack_positions"]:
-			var dist = troop["pos"].distance_to(attack_pos)
-			if dist < min_dist:
-				min_dist = dist
-		score += int(10 - min_dist)
-	return score
-
-func score_statue_damage(game_state) -> int:
-	var score = 0
-	var player_statue_hp = game_state["player_statue"]["hp"]
-	var initial_player_statue_hp = player_statue.current_life_points
-	var damage_to_player_statue = initial_player_statue_hp - player_statue_hp
-	score += damage_to_player_statue * 50
-	if damage_to_player_statue > 0:
-		print(score)
-
-	var enemy_statue_hp = game_state["enemy_statue"]["hp"]
-	var initial_enemy_statue_hp = enemy_statue.current_life_points
-	var damage_to_enemy_statue = initial_enemy_statue_hp - enemy_statue_hp
-	score -= damage_to_enemy_statue * 50
-	return score
-
-func score_victory_defeat(game_state) -> int:
-	var score = 0
-	var player_statue_hp = game_state["player_statue"]["hp"]
-	var enemy_statue_hp = game_state["enemy_statue"]["hp"]
-	if player_statue_hp <= 0:
-		score += 10000
-	elif enemy_statue_hp <= 0:
-		score -= 10000
-	return score
-
-func score_hunt_weaker_targets(game_state) -> int:
-	var score = 0
-	for enemy_troops in game_state["enemy_troops"]:
-		if enemy_troops["hp"] <= 0:
-			continue
-		for player_troops in game_state["player_troops"]:
-			if player_troops["hp"] <= 0:
-				continue
-			# Se o inimigo é mais forte que o player (ataque maior que a vida do player)
-			if enemy_troops["attack_points"] >= player_troops["hp"]:
-				var dist = enemy_troops["pos"].distance_to(player_troops["pos"])
-				# Quanto mais perto, maior o score (ajuste o peso conforme desejar)
-				score += int(10 - dist)
-	return score
-
-func score_approach_invader(game_state) -> int:
-	var score = 0
-	# Supondo que o campo do inimigo é a metade superior do grid (ajuste conforme necessário)
-	var grid_mid_x = 0#float(grid_controller.tile_grid.get_used_rect().size.x) / 2.0
-	for player in game_state["player_troops"]:
-		if player["hp"] <= 0:
-			continue
-		if player["pos"].x >= grid_mid_x:
-			# Esta tropa está invadindo o campo do inimigo
-			for enemy in game_state["enemy_troops"]:
-				if enemy["hp"] <= 0:
-					continue
-				var dist = enemy["pos"].distance_to(player["pos"])
-				score += 20 - 4 * dist
-	return score
-
-func score_attack_invader(game_state, move) -> int:
-	var score = 0
-	if move["type"] == "attack":
-		if move["target"] is Statue:
-			return 0
-			
-		var grid_mid_x = 0 #float(grid_controller.tile_grid.get_used_rect().size.x) / 2
-		for target in game_state["player_troops"]:
-			if target["pos"] == move["target"].get_tile_pos() and target["pos"].x >= grid_mid_x:
-				score += 30  # Incentivo para atacar tropas invasoras
-				break
-	return score
-
-func score_escape_weak_from_strong(game_state, move) -> int:
-	var score = 0
-
-	if move["type"] == "move_troop":
-		var old_pos = move["troop"].get_tile_pos()
-		var new_pos = move["tile"]
-		var troop_hp = move["troop"].current_life_points
-		# Considere "fraca" se hp <= 5 (ajuste conforme seu jogo)
-		if troop_hp <= 2:
-			var min_dist_before = INF
-			var min_dist_after = INF
-			for enemy in game_state["enemy_troops"]:
-				if enemy["hp"] <= 0:
-					continue
-				if enemy["attack_points"] >= 3:
-					var dist_before = old_pos.distance_to(enemy["pos"])
-					var dist_after = new_pos.distance_to(enemy["pos"])
-					if dist_before < min_dist_before:
-						min_dist_before = dist_before
-					if dist_after < min_dist_after:
-						min_dist_after = dist_after
-			# Se aumentou a distância, valorize
-			if min_dist_after > min_dist_before:
-				score += int((min_dist_after - min_dist_before) * 10)
-			# Se diminuiu, puna
-			elif min_dist_after < min_dist_before:
-				score -= int((min_dist_before - min_dist_after) * 10)
-	return score
-
-func score_approach_enemy_defense_statue(game_state) -> int:
-	var score = 0
-	var defense_positions = game_state["enemy_statue"]["attack_positions"]
-	for player_troop in game_state["player_troops"]:
-		if player_troop["hp"] <= 0:
-			continue
-		# Se o inimigo está em uma posição de ataque à estátua
-		if player_troop["pos"] in defense_positions:
-			for enemy_troop in game_state["enemy_troops"]:
-				if enemy_troop["hp"] <= 0:
-					continue
-				var dist = enemy_troop["pos"].distance_to(player_troop["pos"])
-				score += int(12 - dist) # Quanto mais perto, maior o score
-	return score
-
-func score_kill_enemy_defense_statue(game_state, move) -> int:
-	var score = 0
-	
-	if move["type"] == "attack":
-		if move["target"] is Statue:
-			return 0
-		
-		var defense_positions = game_state["enemy_statue"]["attack_positions"]
-		for target in game_state["player_troops"]:
-			
-			if target["pos"] == move["target"].get_tile_pos() and target["pos"] in defense_positions:
-				if target["hp"] <= 0:
-					score += 50 # Valorize matar inimigo que ameaçava a estátua
-	return score
-
-
-func get_valid_spawn_tiles():
+func get_valid_spawn_tiles() -> Array:
 	return ai_spawner.get_valid_spawn_tiles()
 
-func get_valid_move_tiles(troop: MobileTroop):
+func get_valid_move_tiles(troop: MobileTroop) -> Array:
 	return grid_controller.get_valid_move_tiles(troop)
 
 func get_attackable_targets(troop: MobileTroop) -> Array[Entity]:
 	var targets = ai_finder.get_attackable_targets(troop)
 	var attack_player_positions = ai_finder.get_attack_player_tiles()
+	
 	for pos in attack_player_positions:
 		if troop.get_tile_pos() == pos:
 			targets.append(player_statue)
-			
-	
 	
 	return targets
-	
-func can_play_the_card(card):
+
+func can_play_the_card(card) -> bool:
 	return card_manager.can_play_the_card(card, Entity.EntityFaction.ENEMY)
