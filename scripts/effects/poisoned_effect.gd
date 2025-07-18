21f8ff9726fa85ff7be736eb58f7456703c78934
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-18 17:50:58 -0300

    change skills to add a image

--- scripts/effects/poisoned_effect.gd
+++ scripts/effects/poisoned_effect.gd
@@ -3,13 +3,15 @@ extends Effect
 class_name PoisonedEffect
 
 const POISON_PARTICLE = preload("res://particles/poison_particle.tscn")
+const CLOUD_POISON_0 = preload("res://textures/crawl-tiles Oct-5-2010/effect/cloud_poison0.png")
+
 var entity : Entity
 
 func _init() -> void:
-	super._init("Poisoned", 3, Moment.END_TURN)
+	super._init("Poisoned", 3, Moment.END_TURN, CLOUD_POISON_0)
 
-func apply_effect(entity : Entity):
-	self.entity = entity
+func apply_effect(_entity : Entity):
+	self.entity = _entity
 	self.entity.take_damage(1)
 	spawn_poison_particle()
 	print("%s lost 1 life by poison!" % entity.name)
--- scripts/skills/skill.gd
+++ scripts/skills/skill.gd
@@ -2,12 +2,22 @@ extends Node
 
 class_name Skill
 
+var skill_name : String
+var description : String
+var image : Texture2D
+
 enum Type{
 	ATTACK,
 	SPAWN,
 	AREA
 }
 var type : Type
+
+func _init(_skill_name : String, _description : String, _image : Texture2D) -> void:
+	self.skill_name = _skill_name
+	self.description = _description
+	self.image = _image
+	
 
 func activate(_target : Entity):
 	pass
