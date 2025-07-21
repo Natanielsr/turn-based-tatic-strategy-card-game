6f68c35b594a0de207018b108aaa194da5a4daf2
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-21 17:05:39 -0300

    bleeding effect

---
+++ scripts/effects/bleeding_effect.gd
@@ -0,0 +1,17 @@
+extends Effect
+
+class_name BleedingEffect
+
+const BLEEDING_PARTICLE = preload("res://particles/blood_particle.tscn")
+const BLOOD_RED_3 = preload("res://textures/crawl-tiles Oct-5-2010/dc-misc/blood_red3.png")
+
+var entity : Entity
+
+func _init() -> void:
+	super._init("Bleeding", 1, Moment.START_TURN, BLOOD_RED_3, BLEEDING_PARTICLE)
+
+func apply_effect(_entity : Entity):
+	self.entity = _entity
+	self.entity.take_damage(2) #DAMAGE
+	spawn_particle(_entity.global_position)
+	print("%s lost 2 life by bleeding!" % entity.name)
