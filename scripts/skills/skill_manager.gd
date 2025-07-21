76ae9d575c35a832c5526910c07a63f8e725e652
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-21 17:07:49 -0300

    skill manager add skill name from file

--- scripts/skills/skill_manager.gd
+++ scripts/skills/skill_manager.gd
@@ -3,6 +3,10 @@ extends Node
 class_name SkillManager
 
 var skills : Array[Skill]
+var file_manager : FileManager
+
+func _init(_file_manager : FileManager) -> void:
+	file_manager = _file_manager
 
 func add(skill : Skill):
 	skills.append(skill)
@@ -11,3 +15,15 @@ func activate_skill_attack(target : Entity):
 	for skill : Skill in skills:
 		if skill.type == Skill.Type.ATTACK:
 			skill.activate(target)
+
+func add_skill_name(skill_name : String):
+	if skill_name != "":
+		var script_path = file_manager.get_skill_script_path(skill_name)
+		var script = load(script_path)
+		if script:
+			var skill_instance = script.new()
+			add(skill_instance)
+			print("Instance: ", skill_instance.name)
+		else:
+			push_error("SKILL SCRIPT CANT LOAD")
+		
