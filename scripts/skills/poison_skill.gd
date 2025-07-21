2f8e6dd18925c75562f5267275f88f7b1f3531e9
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-21 17:07:30 -0300

    poison skill particle moved to base

--- scripts/skills/poison_skill.gd
+++ scripts/skills/poison_skill.gd
@@ -7,17 +7,16 @@ const CLOUD_POISON_0 = preload("res://textures/crawl-tiles Oct-5-2010/effect/clo
 var target : Entity
 
 func _init() -> void:
-	super._init("Poison", "Deal 1 Damage in the end of turn", CLOUD_POISON_0)
+	super._init(
+		"Poison",
+		"Deal 1 Damage against opponent in the end of opponent turn",
+		 CLOUD_POISON_0,
+		POISON_PARTICLE
+		)
 	
 func activate(_target : Entity):
 	self.target = _target
 	var effect = PoisonedEffect.new()
 	self.target.effects_manager.add_effect(effect)
-	spawn_poison_particle()
+	spawn_particle(target.global_position)
 	
-
-func spawn_poison_particle():
-	var part = POISON_PARTICLE.instantiate()
-	self.target.add_child(part)
-	part.global_position = target.global_position
-	part.emitting = true
