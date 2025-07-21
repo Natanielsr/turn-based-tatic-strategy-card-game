eae1d00a986d36905b4a230f5addc69bb9d09f49
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-21 17:06:09 -0300

    remove particle from poison effect and add to base

--- scripts/effects/poisoned_effect.gd
+++ scripts/effects/poisoned_effect.gd
@@ -8,16 +8,10 @@ const CLOUD_POISON_0 = preload("res://textures/crawl-tiles Oct-5-2010/effect/clo
 var entity : Entity
 
 func _init() -> void:
-	super._init("Poisoned", 3, Moment.END_TURN, CLOUD_POISON_0)
+	super._init("Poisoned", 3, Moment.END_TURN, CLOUD_POISON_0, POISON_PARTICLE)
 
 func apply_effect(_entity : Entity):
 	self.entity = _entity
 	self.entity.take_damage(1)
-	spawn_poison_particle()
+	spawn_particle(_entity.global_position)
 	print("%s lost 1 life by poison!" % entity.name)
-
-func spawn_poison_particle():
-	var part = POISON_PARTICLE.instantiate()
-	self.entity.add_child(part)
-	part.global_position = entity.global_position
-	part.emitting = true
