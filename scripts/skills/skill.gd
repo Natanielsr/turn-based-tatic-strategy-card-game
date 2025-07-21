4336247a595171ffbb18b7882ec048b5c8b92529
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-21 17:08:00 -0300

    skill particle in base

--- scripts/skills/skill.gd
+++ scripts/skills/skill.gd
@@ -5,6 +5,7 @@ class_name Skill
 var skill_name : String
 var description : String
 var image : Texture2D
+var particle
 
 enum Type{
 	ATTACK,
@@ -13,11 +14,25 @@ enum Type{
 }
 var type : Type
 
-func _init(_skill_name : String, _description : String, _image : Texture2D) -> void:
+func _init(
+	_skill_name : String,
+	_description : String,
+	_image : Texture2D,
+	_particle
+	) -> void:
+		
 	self.skill_name = _skill_name
 	self.description = _description
 	self.image = _image
+	self.particle = _particle
+	
 	
 
 func activate(_target : Entity):
 	pass
+	
+func spawn_particle(pos : Vector2):
+	var part = particle.instantiate()
+	self.target.add_child(part)
+	part.global_position = pos
+	part.emitting = true
