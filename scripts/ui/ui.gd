958d2bc717550ee17b76ff01036d1864f81eefdc
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-25 12:31:00 -0300

    change mouse style by signals

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -4,7 +4,7 @@ class_name UI
 
 @onready var game_controller: GameController = get_node("/root/Base/Controllers/GameController")
 @onready var turn_controller: TurnController = $"../Controllers/TurnController"
-
+@onready var troop_manager: TroopManager = $"../TroopManager"
 
 const Turn = TurnController.Turn
 
@@ -15,11 +15,45 @@ const cursor_attack_texture = preload("res://textures/crawl-tiles Oct-5-2010/ite
 const TRAVEL_EXCLUSION_CENTRE = preload("res://textures/crawl-tiles Oct-5-2010/dc-misc/travel_exclusion_centre.png")
 const BOOTS_1_BROWN = preload("res://textures/crawl-tiles Oct-5-2010/item/armour/boots1_brown.png")
 
-
+enum MouseStyle{
+	NORMAL,
+	MOVE,
+	ATTACK,
+	CANCEL
+}
+var current_mouse_style = MouseStyle.NORMAL
+var current_troop_select
 
 func _ready() -> void:
 	turn_controller.connect("changed_turn", Callable(self, "_on_changed_turn"))
+	game_controller.on_select_tropp.connect(_on_select_troop)
+	game_controller.on_deselect_troop.connect(_on_deselect_troop)
+	troop_manager.mouse_on_troop.connect(_mouse_on_troop)
+	troop_manager.mouse_left_troop.connect(_mouse_left_troop)
 	set_turn(turn_controller.turn)
+	
+func _on_select_troop(_troop):
+	cursor_boots()
+	
+func _on_deselect_troop(_troop):
+	cursor_normal()
+	
+func _mouse_on_troop(troop):
+	if troop.faction == Entity.EntityFaction.ENEMY:
+		cursor_attack()
+	else:
+		cursor_normal()
+		
+	current_troop_select = troop
+	
+func _mouse_left_troop(troop):
+	if troop == current_troop_select:
+		if game_controller.selected_troop:
+			cursor_boots()
+		else:
+			cursor_normal()
+		
+		current_troop_select = null
 	
 func _on_changed_turn(turn: GameController.Turn):
 	set_turn(turn)
@@ -33,32 +67,7 @@ func set_turn(turn):
 	else:
 		finish_turn_btn.disabled = true
 		enemy_finish_turn_btn.disabled = false
-	
-func _process(_delta: float) -> void:
-	show_effects()
-	
-	if game_controller.current_game_state != GameController.GameState.RUNNING:
-		cursor_normal()
-		return
-	
-	if not game_controller.selected_troop:
-		cursor_normal()
-		return
-		
-	if enemy_on_mouse():
-		if game_controller.selected_troop.get_attack_count() > 0:
-			cursor_attack()
-		else:
-			cursor_cancel()
-	else:
-		if not game_controller.selected_troop.is_exausted:
-			cursor_boots()
-		else:
-			cursor_cancel()
 			
-func show_effects():
-	
-	pass
 	
 func enemy_on_mouse() -> Entity:
 	var mouse_pos = get_global_mouse_position()
@@ -89,12 +98,20 @@ func _on_enemy_finish_turn_pressed() -> void:
 		
 func cursor_normal():
 	Input.set_custom_mouse_cursor(null)
+	current_mouse_style = MouseStyle.NORMAL
+	return current_mouse_style
 	
 func cursor_cancel():
 	Input.set_custom_mouse_cursor(TRAVEL_EXCLUSION_CENTRE, Input.CURSOR_ARROW, Vector2(16, 16))
+	current_mouse_style = MouseStyle.CANCEL
+	return current_mouse_style
 	
 func cursor_attack():
 	Input.set_custom_mouse_cursor(cursor_attack_texture, Input.CURSOR_ARROW, Vector2(16, 16))
+	current_mouse_style = MouseStyle.ATTACK
+	return current_mouse_style
 	
 func cursor_boots():
 	Input.set_custom_mouse_cursor(BOOTS_1_BROWN, Input.CURSOR_ARROW, Vector2(16, 16))
+	current_mouse_style = MouseStyle.MOVE
+	return current_mouse_style
