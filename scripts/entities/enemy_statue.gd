fbc1f868d4721f05f69a8b3cd5bded36d16a1d64
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-25 12:29:32 -0300

    mouse over signal enemy statue

--- scripts/entities/enemy_statue.gd
+++ scripts/entities/enemy_statue.gd
@@ -1,6 +1,8 @@
 extends Statue
 
 class_name EnemyStatue
+signal mouse_on(entity)
+signal mouse_left(entity)
 
 signal enemy_die()
 
@@ -16,6 +18,10 @@ func get_distance(pos : Vector2):
 	if faction_area == EntityFaction.ENEMY: #ENEMY AREA
 		return 1
 	else:
-		return 9999
+		return INF
 	
+func _on_mouse_entered() -> void:
+	emit_signal("mouse_on", self)
 	
+func _on_mouse_exited() -> void:
+	emit_signal("mouse_left", self)
