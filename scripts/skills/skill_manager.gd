e6e7366fb135aef72ff98a960af9d313f5165fbf
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-29 17:46:00 -0300

    add skills , skill owner, activate skill method, check is provoker skill, change name parameter from get skill path by name method, and add methods to siginals attack and died trigger in skill manager

--- scripts/skills/skill_manager.gd
+++ scripts/skills/skill_manager.gd
@@ -3,22 +3,26 @@ extends Node
 class_name SkillManager
 
 var skills : Array[Skill]
-var file_manager : FileManager
+var skill_owner : Entity
 
 const SKILL_PATHS = [
 	"res://scripts/skills/bleeding_skill.gd",
-	"res://scripts/skills/poison_skill.gd"
+	"res://scripts/skills/poison_skill.gd",
+	"res://scripts/skills/plague_skill.gd",
+	"res://scripts/skills/provoke_skill.gd",
 ]
 
-func _init(_file_manager : FileManager) -> void:
-	file_manager = _file_manager
+func _init(_skill_owner : Entity) -> void:
+	skill_owner = _skill_owner
+	skill_owner.attack_finished.connect(_on_attack_trigger)
+	skill_owner.died.connect(_on_died_trigger)
 
 func add(skill : Skill):
 	skills.append(skill)
-	
-func activate_skill_attack(target : Entity):
+			
+func activate_skill(target: Entity, skill_type : Skill.Type):
 	for skill : Skill in skills:
-		if skill.type == Skill.Type.ATTACK:
+		if skill.type == skill_type:
 			skill.activate(target)
 
 func add_skill_name(skill_name : String):
@@ -28,18 +32,26 @@ func add_skill_name(skill_name : String):
 		var script = load(script_path)
 		if script:
 			var skill_instance = script.new()
+			if skill_instance is ProvokeSkill:
+				skill_instance.set_provoker(skill_owner)
 			add(skill_instance)
 			print("Instance: ", skill_instance.name)
 		else:
 			push_error("SKILL SCRIPT CANT LOAD")
 		
-func get_skill_path_by_name(name: String) -> String:
-	name = name.strip_edges().to_lower()
+func get_skill_path_by_name(_name: String) -> String:
+	_name = _name.strip_edges().to_lower()
 	for path in SKILL_PATHS:
 		var file_name = path.get_file() # exemplo: poison_skill.gd
-		if file_name.begins_with(name + "_"):
+		if file_name.begins_with(_name + "_"):
 			return path
 	
 	push_error("SKILL NAME NOT FOUND IN SKILL PATH")	
 	return ""
 	
+func _on_attack_trigger(_attacker : Entity, _target : Entity):
+	activate_skill(_target, Skill.Type.ATTACK)
+	
+func _on_died_trigger(_dead_entity : Entity, killed_by : Entity):
+	if not _dead_entity.is_attacking:
+		activate_skill(killed_by, Skill.Type.DEATH)
