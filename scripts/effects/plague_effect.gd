8061f418a8a31c30a5c5058fccedc1e1c7a5aeba
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-07-29 17:38:19 -0300

    create plague effect

---
+++ scripts/effects/plague_effect.gd
@@ -0,0 +1,17 @@
+extends Effect
+
+class_name PlagueEffect
+
+const CLOUD_BLACK_SMOKE = preload("res://textures/crawl-tiles Oct-5-2010/effect/cloud_black_smoke.png")
+const PLAGUE_PARTICLE = preload("res://particles/plague_particle.tscn")
+
+var entity : Entity
+
+func _init() -> void:
+	super._init("Plague", 3, Moment.START_TURN, CLOUD_BLACK_SMOKE, PLAGUE_PARTICLE)
+
+func apply_effect(_entity : Entity):
+	self.entity = _entity
+	self.entity.take_damage(1)
+	spawn_particle(_entity.global_position)
+	print("%s lost 1 life by poison!" % entity.name)
