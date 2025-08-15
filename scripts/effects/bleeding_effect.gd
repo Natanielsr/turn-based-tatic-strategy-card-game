71fcdf21646be21569a006e44f4d5e6912214359
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-08-15 12:27:24 -0300

    add sound to bleeding effect

--- scripts/effects/bleeding_effect.gd
+++ scripts/effects/bleeding_effect.gd
@@ -4,7 +4,7 @@ class_name BleedingEffect
 
 const BLEEDING_PARTICLE = preload("res://particles/blood_particle.tscn")
 const BLOOD_RED_3 = preload("res://textures/crawl-tiles Oct-5-2010/dc-misc/blood_red3.png")
-
+const BLOOD = preload("res://sounds/blood.wav")
 
 func _init() -> void:
 	super._init("Bleeding", 1, Moment.START_TURN, BLOOD_RED_3, BLEEDING_PARTICLE)
@@ -12,5 +12,6 @@ func _init() -> void:
 func apply_effect(_entity : Entity):
 	self.entity = _entity
 	self.entity.take_damage(2) #DAMAGE
+	sound_fx.play_temp_sound_with_volume(BLOOD, _entity.position, 15)
 	spawn_particle(_entity.global_position)
 	print("BleedingEffect > apply_effect: %s lost 2 life by bleeding!" % entity.name)
