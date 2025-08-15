d52943e7dbde1f0ec3ea131328fff5a527c28095
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-08-15 12:29:51 -0300

    add sound to bleeding skill

--- scripts/skills/bleeding_skill.gd
+++ scripts/skills/bleeding_skill.gd
@@ -4,6 +4,7 @@ class_name BleedingSkill
 
 const BLEEDING_PARTICLE = preload("res://particles/blood_particle.tscn")
 const BLOOD_RED_3 = preload("res://textures/crawl-tiles Oct-5-2010/dc-misc/blood_red3.png")
+const BLOOD = preload("res://sounds/blood.wav")
 
 func _init(_skill_owner : Entity) -> void:
 	super._init(
@@ -19,5 +20,7 @@ func activate(_target : Entity):
 	self.target = _target
 	var effect = BleedingEffect.new()
 	self.target.effects_manager.add_effect(effect)
+	
+	sound_fx.play_temp_sound_with_volume(BLOOD, _target.position, 15)
 	spawn_particle(target.global_position)
 	
