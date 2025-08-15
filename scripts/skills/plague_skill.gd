b4e0f5f2ef764145d379a964451c7a5b171c05f0
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-08-15 12:30:05 -0300

    add sound to plague skill

--- scripts/skills/plague_skill.gd
+++ scripts/skills/plague_skill.gd
@@ -4,6 +4,7 @@ class_name Plague
 
 const PLAGUE_PARTICLE = preload("res://particles/plague_particle.tscn")
 const CLOUD_BLACK_SMOKE = preload("res://textures/crawl-tiles Oct-5-2010/effect/cloud_black_smoke.png")
+const PLAGUE_3 = preload("res://sounds/plague3.ogg")
 
 func _init(_skill_owner : Entity) -> void:
 	super._init(
@@ -19,6 +20,7 @@ func activate(_target : Entity):
 	self.target = _target
 	var effect = PlagueEffect.new()
 	if target != null:
+		sound_fx.play_temp_sound(PLAGUE_3, _target.position)
 		self.target.effects_manager.add_effect(effect)
 		spawn_particle(target.global_position)
 	
