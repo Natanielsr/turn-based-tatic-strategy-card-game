adcba55d5bd2cf18bca8e4abcf4ce6f9519dbf5c
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-08-20 12:07:20 -0300

    choose start cards

--- scripts/card_system/choose_start_cards.gd
+++ scripts/card_system/choose_start_cards.gd
@@ -1,3 +1,44 @@
 extends Node2D
 
 class_name ChooseStartCards
+
+var deck: Deck
+var hand: Hand 
+
+@onready var sound_fx: SoundFX = get_node("/root/Base/Sound/SoundFX")
+const CUCKOO = preload("res://sounds/Cardsounds/cockatrice/cuckoo.wav")
+
+var cards = []
+var selected_cards = []
+
+func set_deck_and_hand(_deck: Deck, _hand: Hand) -> void:
+	self.deck = _deck
+	self.hand = _hand
+
+func _ready() -> void:
+	choose()
+
+func choose():
+	for i in range(3):
+		await Waiter.wait(0.2)
+		var card : Card = deck.draw_card()
+		cards.append(card)
+		
+func mark_a_card(card : Card):
+	if card not in selected_cards:
+		card.get_node("RemoveSprite").visible = true
+		selected_cards.append(card)
+	else:
+		card.get_node("RemoveSprite").visible = false
+		selected_cards.erase(card)
+
+func confirm_choose() -> void:
+	
+	for card : Card in selected_cards:
+		hand.remove_card_from_hand(card)
+		card.queue_free()
+		
+	deck.draw_cards(selected_cards.size())
+		
+	sound_fx.play_temp_sound(CUCKOO, self.position)
+	hand.update_hand_position()
